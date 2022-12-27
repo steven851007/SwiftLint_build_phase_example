c219e19fe056e04de9290b42b7319481383fdfab
Istvan Balogh <istvan.balogh@twint.ch>
2022-12-27 14:45:00 +0700

    Initial Commit

---
+++ SwiftlintBuildPhase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftlintBuildPhase
+//
+//  Created by Istvan Balogh on 27.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
